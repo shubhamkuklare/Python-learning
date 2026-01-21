36f47bcdb6dc87437982be609edec72889f94946
shubhamkuklare <shubhamkuklare943@gmail.com>
2026-01-21 12:53:50 +0530

    learn python

---
+++ day.py
@@ -0,0 +1,20 @@
+# Day program using user input
+
+day = int(input("Enter a number (1-7): "))
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("Invalid input! Please enter a number between 1 and 7.")
